f23f890c68f3a40c94ff93585c33c3b9828d372a
Akira Ohgaki <akiraohgaki@gmail.com>
2018-06-16 02:51:45 +0900

    Update ts files

--- i18n/messages.ts
+++ i18n/messages.ts
@@ -4,107 +4,112 @@
 <context>
     <name>ItemHandler</name>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="55"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="53"/>
+        <source>The item already installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="62"/>
         <source>The file already downloading</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="69"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="76"/>
         <source>Downloading</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="119"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="125"/>
         <source>Invalid OCS-URL</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="128"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="136"/>
+        <source>The item not installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="142"/>
         <source>Uninstalling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="197"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="212"/>
         <source>Uninstalled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="222"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="237"/>
         <source>Downloaded</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="252"/>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="291"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="267"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="311"/>
         <source>Saving</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="264"/>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="306"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="284"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="328"/>
         <source>Failed to save data</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="271"/>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="314"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="291"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="336"/>
         <source>Saved</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="319"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="341"/>
         <source>Installing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="330"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="352"/>
         <source>The file has been installed as program</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="335"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="357"/>
         <source>The plasmoid has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="340"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="362"/>
         <source>The plasma look and feel has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="345"/>
-        <source>The plasma desktop theme has been installed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="350"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="372"/>
         <source>The KWin effect has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="355"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="377"/>
         <source>The KWin script has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="360"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="382"/>
         <source>The KWin window switcher has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="363"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="385"/>
         <source>The archive file has been extracted</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="366"/>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="380"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="388"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="402"/>
         <source>The file has been installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="370"/>
-        <location filename="../app/src/handlers/itemhandler.cpp" line="384"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="392"/>
+        <location filename="../app/src/handlers/itemhandler.cpp" line="406"/>
         <source>Failed to installation</source>
         <translation type="unfinished"></translation>
     </message>
